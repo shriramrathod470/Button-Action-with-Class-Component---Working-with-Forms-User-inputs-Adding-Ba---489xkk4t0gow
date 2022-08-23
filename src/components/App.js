4de1616ab182041997cfb5c23def94fa45afe2aa
shriramrathod470 <shriramrathod47@gmail.com>
2022-08-23 11:25:18 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,14 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
-		para:''
 	};
-
-	 paragraph = ()  => {
-		this.useState({
-			para:`<p id= para>  Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
-			</p>`
-		})
-	}
 
     render() {
     	return(
     		<div id="main">
 				{ /* Do not remove this main div!! */ }
 				{this.state.para}
-			<button  id ="click" onClick={this.paragraph} >onclick</button>
+			<button  id ="click" onClick={<P id = "para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</P>} >Display</button>
     		</div>
     	);
     }
