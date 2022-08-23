64af283e5999bbc6eee29b5631536f01b5ab2173
shriramrathod470 <shriramrathod47@gmail.com>
2022-08-23 10:54:08 +0530

    edited

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,15 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+		
 	};
 
     render() {
     	return(
     		<div id="main">
 				{ /* Do not remove this main div!! */ }
+				<button  id ="click" onClick={<p id = "para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+</p>} >paragraph</button>
     		</div>
     	);
     }
